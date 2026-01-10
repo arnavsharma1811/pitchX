ac093c6bd61b4ee639d1c8a81914c375770e99e2
arnavsharma1811 <arnavsharmaone8@gmail.com>
2026-01-10 23:49:02 +0530

    homepage setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
-
-
+import Link from "next/link";
+import Image from "next/image";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
-      <body className="mona_sans antialiased pattern">
-        {children}
-      </body>
-    </html>
+    <div className="root-layout">
+      <nav>
+        <Link href = "/" className = "flex items-center gap-2" >
+        <Image src = "/logo.svg" alt = "Logo" width = {38}
+        height = {32}/>
+        <h2 className="text-primary-100">PitchX</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+
 const page = () => {
   return(
-    <div>
-      home page
+   <>
+   <section className="card-cta" >
+     <div className = "flex flex-col gap-6 max-w-lg" >
+      <h2>Get Interview-ready with AI-powered practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get intsant feedback
+          </p>
+           
+           <Button asChild className="btn-primary max-sm:w-full">
+            <Link href = "/interview">Start an Interview</Link>
+           </Button>
+
+     </div>
+     <Image src = "/robot.png" alt = "robo-dude" width = {400} height = {400} className="max-sm:hidden" />
+   </section>
+   
+   <section className="flex flex-col gap-6 mt-8">
+    <h2>Your Interviews</h2>
+    <div className="interviews-section">
+      <p>You haven&apos;t taken any interviews yest</p>
     </div>
+   </section>
+   <section className="flex felx-col gap-6 mt-8">
+    <h2>Take an interview</h2>
+    <div className="interviews-section">
+      <p>There are no interviews available</p>
+    </div>
+   </section>
+
+   </>
   )
 }
 
